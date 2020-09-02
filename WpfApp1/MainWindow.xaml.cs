fefe563f1c65a7c8534c495052671ad0cffbaa5c
morimori-corder-toh <67218438+morimori-coder@users.noreply.github.com>
2020-09-02 14:53:52 +0900

    ボタンクリックのイベントハンドラにメッセージ表示を入れた

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -11,5 +11,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
     }
 }
